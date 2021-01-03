f7f46d5c75216bb2241488cc25292799e5ca23fd
Bharti Parmar <38555462+Bharti-Parmar@users.noreply.github.com>
2021-01-03 08:07:45 -0800

    Add files via upload

--- SearchElementFromAPI.js
+++ SearchElementFromAPI.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import axios from 'axios';
 
 function SearchElementFromAPI() {
     
     const [data, setData] = useState({ hits: [] });
-    const [search, setSearch] = useState('react');
-    const [url, setURL] = useState(`https://hn.algolia.com/api/v1/search?query=react`);
+    const [query, setQuery] = useState('redux');
+    const [url, setURL] = useState(`https://hn.algolia.com/api/v1/search?query=redux`,);
     const [isLoading, setLoading] = useState(false);
     const [error, isError] = useState(false);
 
@@ -26,18 +26,18 @@ function SearchElementFromAPI() {
 
 
   return (
-    <>
+    <Fragment>
     {/* Search Box */}
         <form>
             <h2 className="text-success"> My Posts </h2>
             <center>
-                <input type="text" className="form-control col-3" value={search} onChange={e => setSearch(e.target.value)} />
-                <button type="submit" className="btn btn-primary col-1 mt-3 w-50" onClick={() => setURL(`https://hn.algolia.com/api/v1/search?query=${search}`)}>Search</button>
+                <input type="text" className="form-control col-3" value={query} onChange={e => setQuery(e.target.value)} />
+                <button type="submit" className="btn btn-primary col-1 mt-3 w-50" onClick={() => setURL(`https://hn.algolia.com/api/v1/search?query=${query}`)}>Search</button>
             </center>
         </form>
 
     {/*  Show Error */}
-        {error && <div className="text-danger mt-3">Oops... Something Went Wrong... </div>}
+        {error && <div className="h3 text-danger mt-3">Oops... Something Went Wrong... </div>}
 
             {
     //  Loading Page
@@ -46,18 +46,22 @@ function SearchElementFromAPI() {
                     <div className="h3 mt-3 text-danger">Loading...</div> 
                 ) : (
     // Show Data
-                <div className="row mt-3">
+                <div className="row mt-3 mb-3">
                     {
                         data.hits.map(item => (
-                            <div className="col-md-3 mt-2" key={item.objectID}>
-                                <p><h5 className="text-danger">{item.objectID}</h5><a href={item.url}>{item.title}</a></p>
+                            <div className="col-md-3 mt-2 border" key={item.objectID}>
+                                <h5>
+                                    <p className="text-danger">ID : {item.objectID}</p>
+                                    Link : <a href={item.url}>{item.title}</a>
+                                </h5>
+                                <h6>Author Name: {item.author}</h6>
                             </div>
                     ))
                     }
                 </div>
                 )
             }
-    </>
+    </Fragment>
   );
 
 }
